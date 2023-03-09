4695908bfe23c9ff3d5f6498a1ff1e94c7459621
Junxiao <zhao_junxiao@hotmail.com>
2023-03-09 17:54:45 -0500

    front end category 3&4 finish

--- front_end.py
+++ front_end.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 # The player is only at one place at a time
 def categ1(nodes: list, num_steps: int):
     proposition = "~At({},%d) V ~At({},%d)\n"
-    propos = list()  # store the proposition between nodes
+    propos = list()  # store the propositions
 
     # format and add the pairs
     for i, n1 in enumerate(nodes):
@@ -25,7 +25,7 @@ def categ1(nodes: list, num_steps: int):
 def categ2(nodes: list, adj: defaultdict, num_steps: int):
     proposition_l = "~At({},%d)"  # left part at position i
     proposition_r = " V At({},%d)"  # right part at position i+1
-    propos = list()  # store the proposition between nodes
+    propos = list()  # store the propositions
 
     # format and add proposition
     for n1 in nodes:
@@ -38,8 +38,48 @@ def categ2(nodes: list, adj: defaultdict, num_steps: int):
     outputs = list()
     for i in range(num_steps):
         for p in propos:
-            val = i if p[0] == "~" else i + 1
+            val = i if p[0] == "~" else i + 1  # left at i, right at i+1
             outputs.append(p % val)
+
+    return outputs
+
+
+# Suppose that treasure T is located at node N.
+# Then if the player is at N at time I,
+# then at time I the player has T
+def categ3(nodes: list, adj: defaultdict, num_steps: int):
+    proposition = "~At({},%d) V Has({},%d)\n"
+    propos = list()  # store the propositions
+
+    # format and add proposition
+    for n in nodes:
+        for t in adj[n]['TREASURES']:
+            propos.append(proposition.format(n, t))
+
+    # duplicate and replace the position
+    outputs = list()
+    for i in range(num_steps + 1):
+        for p in propos:
+            outputs.append(p % (i, i))
+
+    return outputs
+
+
+# If the player has treasure T at time I-1,
+# then the player has T at time I
+def categ4(treasures: list, num_steps: int):
+    proposition = "~Has({t},%d) V Has({t},%d)\n"
+    propos = list()  # store the propositions
+
+    # format and add proposition
+    for t in treasures:
+        propos.append(proposition.format(t=t))
+
+    # # duplicate and replace the position
+    outputs = list()
+    for i in range(num_steps):
+        for p in propos:
+            outputs.append(p % (i, i + 1))
 
     return outputs
 
@@ -71,6 +111,24 @@ def read_front(path: str):
     return nodes, treasures, num_steps, adj
 
 
+# write the output file
+def write_front(path: str, outputs: list):
+    try:
+        f = open(path, "w", encoding="utf-8-sig")
+        f.writelines(outputs)
+        f.close()
+
+    except Exception as e:
+        print(e)
+
+
 if __name__ == "__main__":
+    outputs = list()
     nodes, treasures, num_steps, adj = read_front("FrontEndInput.txt")
-    print(categ2(nodes, adj, num_steps))
+
+    outputs += categ1(nodes, num_steps)
+    outputs += categ2(nodes, adj, num_steps)
+    outputs += categ3(nodes, adj, num_steps)
+    outputs += categ4(treasures, num_steps)
+
+    write_front("FrontEndOutput.txt", outputs)
